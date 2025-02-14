f747cded71d41de07749ad6f6f8c7de43cfd7f44
Artem Arafailov <artem.ar888@gmail.com>
2025-02-14 17:13:45 +0300

    Initial commit

---
+++ src/test/java/ru/netology/rest/postmanecho/PostmanEchoTest.java
@@ -0,0 +1,29 @@
+package ru.netology.rest.postmanecho;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+/*
+    @Test
+    void shouldReturnBody() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8") // если текст не на латинице
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+        ;
+    }
+
+*/
+}
