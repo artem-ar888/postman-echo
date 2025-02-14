8aafaf9c83fe326d1a6a24f53c5154bd79a198ea
Artem Arafailov <artem.ar888@gmail.com>
2025-02-14 18:05:01 +0300

    Add tests

--- src/test/java/ru/netology/rest/postmanecho/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/postmanecho/PostmanEchoTest.java
@@ -8,20 +8,36 @@ import static org.hamcrest.Matchers.equalTo;
 public class PostmanEchoTest {
 
     @Test
-    void shouldReturnBody() {
+    public void shouldReturnBodyWithPost() {
         // Given - When - Then
         // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8") // если текст не на латинице
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("данные") // отправляемые данные (заголовки и query можно выставлять аналогично)
                 // Выполняемые действия
                 .when()
                 .post("/post")
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("данные"))
+        ;
+    }
+
+    @Test
+    public void shouldReturnBodyWithGet() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                // Выполняемые действия
+                .when()
+                .get("/get?foo1=bar1&foo2=bar2")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("args.foo1", equalTo("bar1"))
         ;
     }
 }
